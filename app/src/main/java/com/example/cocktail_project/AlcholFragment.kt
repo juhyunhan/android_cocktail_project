d3d02c5650086469ebcc28627263e13f8c292789
vvontomi <vvontomato@gmail.com>
2022-12-05 22:17:19 +0900

    recycle view

--- app/src/main/java/com/example/cocktail_project/AlcholFragment.kt
+++ app/src/main/java/com/example/cocktail_project/AlcholFragment.kt
@@ -8,7 +8,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.GridLayoutManager
+import com.bumptech.glide.Glide
 import com.example.cocktail_project.databinding.FragmentAlcholBinding
+import com.example.cocktail_project.databinding.FragmentDrinkBinding
+import com.example.cocktail_project.repository.Item
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_alchol.*
+import kotlinx.android.synthetic.main.fragment_recipe.*
 
 
 class AlcholFragment : Fragment() {
@@ -21,12 +33,16 @@ class AlcholFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?, ): View? {
-        var root_view = inflater.inflate(R.layout.fragment_alchol, container, false)
-        var card_view = root_view.findViewById<CardView>(R.id.alchol_cardview_1)
+        //var root_view = inflater.inflate(R.layout.fragment_alchol, container, false)
+        _binding = FragmentAlcholBinding.inflate(inflater)
+
+
+        return _binding?.root
+        /*var card_view = root_view.findViewById<CardView>(R.id.alchol_cardview_1)
 //        for card in car_views:
 //            card.apply{
 //
-//
+//                ...
 //            }
         card_view.apply{
             setOnLongClickListener { v ->
@@ -61,6 +77,74 @@ class AlcholFragment : Fragment() {
 
         }
         // Inflate the layout for this fragment
+
+        /*val database = Firebase.database
+        val myRef = database.getReference("base")
+        //var base: String = "aa"
+
+        val postListener = object : ValueEventListener {
+            //private lateinit var textView: TextView
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+
+                val base = dataSnapshot.getValue<String>()!!
+                Glide.with(this@AlcholFragment)
+                    .load(base)
+                    .into(b1)
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+
+            }
+        }
+        myRef.addValueEventListener(postListener)*/
+
         return root_view
+    }*/
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+        val databas = Firebase.database
+        val itemRef = databas.getReference("base")
+        val arraylist = ArrayList<Item>()
+
+        binding.recbase.setHasFixedSize(true)
+        _binding?.recbase?.layoutManager =  GridLayoutManager(context,3)
+        _binding?.recbase?.adapter = Adapter(arraylist)
+
+
+
+
+        itemRef.addListenerForSingleValueEvent(object : ValueEventListener {
+
+            //@SuppressLint("NotifyDataSetChanged")
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                //var item: Item
+                //arraylist.clear()
+                //var item: Item
+                for (snapshot in dataSnapshot.children) {
+                    val item = snapshot.getValue<Item>()!!
+                    //val item = Item("51759725981598")
+                    //println(item.uu())
+                    arraylist.add(item)
+                }
+
+                _binding?.recbase?.adapter?.notifyDataSetChanged()
+                //_binding?.recdrink?.adapter = Adapter(arraylist)
+                //Adapter(arraylist).notifyDataSetChanged()
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+            }
+
+        })
+
+
     }
 }
--- app/src/main/java/com/example/cocktail_project/DrinkFragment.kt
+++ app/src/main/java/com/example/cocktail_project/DrinkFragment.kt
@@ -5,99 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cocktail_project.databinding.FragmentDrinkBinding
+import com.example.cocktail_project.repository.Item
+import com.example.cocktail_project.viewmodel.CocktailViewModel
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 class DrinkFragment : Fragment() {
 
 
+
     private var _binding: FragmentDrinkBinding? = null
     private val binding get() = _binding!!
-    //val databas = Firebase.database
-    //val itemRef = databas.getReference("drink")
-    //val arraylist = ArrayList<Item>()
-    //var item = Item()
-    //val viewModel : ItemViewModel by viewModels()
 
-    //private lateinit var adapter: Adapter
-    //private val viewModel by lazy { ViewModelProvider(this).get(CocktailViewModel::class.java) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?, ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?, ): View? {
         _binding = FragmentDrinkBinding.inflate(inflater)
 
-        //recyclerView.layoutManager = layoutManager
 
-
-        //var root_view = inflater.inflate(R.layout.fragment_drink, container, false)
-        // Inflate the layout for this fragment
         return _binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //binding.recdrink.setHasFixedSize(true)
 
 
-        //var item = Item()
-        //val recyclerView = binding.recdrink
-        //recyclerView.setHasFixedSize(true)
-        //recyclerView.layoutManager = LinearLayoutManager(requireContext())
-
-        /*itemRef.addValueEventListener(object : ValueEventListener {
-
-            //@SuppressLint("NotifyDataSetChanged")
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                arraylist.clear()
-                for (snapshot: DataSnapshot in dataSnapshot.children) {
-                    item = dataSnapshot.getValue<Item>()!!
-                    //println(item.uu())
-                    arraylist.add(item)
-                }
-                adapter.notifyDataSetChanged()
-            }
-            override fun onCancelled(databaseError: DatabaseError) {
-            }
-        })*/
-        /*val datalist = arrayListOf(
-            Item("aaafsgs"),
-            Item("abb"),
-            Item("accc"),
-            Item("adddd"),
-            Item("sagdga"),
-            Item("asgs"),
-            Item("asdgs"),
-            Item("asdgs")
-        )*/
-        /*viewModel.modify3()
-
-        viewModel.nameArray.observe(viewLifecycleOwner){
-
-            _binding?.recdrink?.adapter?.notifyDataSetChanged()
-        }
-        _binding?.recdrink?.layoutManager =  LinearLayoutManager(context)
-        _binding?.recdrink?.adapter = Adapter(viewModel.nameArray)
-        //_binding?.recdrink?.adapter = Adapter(datalist)*/
-
-
-
-
-        binding.recdrink.setHasFixedSize(true)
-        _binding?.recdrink?.layoutManager = LinearLayoutManager(context)
 
         val databas = Firebase.database
         val itemRef = databas.getReference("drink")
+        val arraylist = ArrayList<Item>()
+
+        binding.recdrink.setHasFixedSize(true)
+        _binding?.recdrink?.layoutManager =  GridLayoutManager(context,3)
+        _binding?.recdrink?.adapter = Adapter(arraylist)
+
+
 
 
         itemRef.addListenerForSingleValueEvent(object : ValueEventListener {
@@ -108,12 +61,13 @@ class DrinkFragment : Fragment() {
                 //arraylist.clear()
                 //var item: Item
                 for (snapshot in dataSnapshot.children) {
-                    //val item = snapshot.getValue<Item>()!!
+                    val item = snapshot.getValue<Item>()!!
                     //val item = Item("51759725981598")
                     //println(item.uu())
-                    //arraylist.add(item)
+                    arraylist.add(item)
                 }
-                //Adapter(arraylist).notifyDataSetChanged()
+
+                _binding?.recdrink?.adapter?.notifyDataSetChanged()
                 //_binding?.recdrink?.adapter = Adapter(arraylist)
                 //Adapter(arraylist).notifyDataSetChanged()
 
@@ -123,28 +77,10 @@ class DrinkFragment : Fragment() {
             }
 
         })
-        /*arraylist.add(Item("sssss"))
-        arraylist.add(Item("sssss"))
-        arraylist.add(Item("sssss"))
-        arraylist.add(Item("sssss"))
-        arraylist.add(Item("sssss"))
-        arraylist.add(Item("sssss"))
-        arraylist.add(Item("sssss"))
-        arraylist.add(Item("sssss"))*/
 
 
-        //_binding?.recdrink?.adapter = Adapter(arraylist)
-        //Adapter(arraylist).notifyDataSetChanged()
-        //_binding?.recdrink?.adapter = Adapter(arraylist)*/
-
-
-        //recyclerView.adapter = Adapter(requireContext(),arraylist)
     }
-}
-/*override fun onDestroyView() {
-    super.onDestroyView()
-    _binding = null
-}
 
 
-}*/
+
+}
